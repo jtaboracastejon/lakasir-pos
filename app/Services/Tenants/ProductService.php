a8cd44cdf9087ecfececcbf4e5b103a58cf95108
Caste <64710811+jtaboracastejon@users.noreply.github.com>
2025-07-25 11:50:02 -0600

    fix: streamline URL handling in handleCreateUploadedFile method

--- app/Services/Tenants/ProductService.php
+++ app/Services/Tenants/ProductService.php
@@ -34,8 +34,7 @@ class ProductService
         foreach ($heroImages as $heroImage => $originalName) {
             $name = $heroImage;
             $url = optional(Storage::disk('public'))->url($name);
-            $urls[] = $url;
-            if (! UploadedFile::where('url', $url)->exists()) {
+            if (!UploadedFile::where('url', $url)->exists()) {
                 UploadedFile::create([
                     'name' => Str::of($heroImage)->replace('product/', ''),
                     'original_name' => $originalName,
@@ -46,9 +45,9 @@ class ProductService
                     'disk' => 'public',
                     'path' => storage_path('app/'.$name),
                 ]);
+                $urls[] = $url;
             }
         }
-
         return $urls;
     }
 
