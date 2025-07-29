70d45a67e8624235da0934390d47153cd68f4673
Caste <64710811+jtaboracastejon@users.noreply.github.com>
2025-07-29 09:18:19 -0600

    fix: update payment denominations for suggested payments in cashier modal

--- resources/views/filament/tenant/pages/cashier.blade.php
+++ resources/views/filament/tenant/pages/cashier.blade.php
@@ -496,7 +496,7 @@ use App\Features\{PaymentShortcutButton, SellingTax, Discount};
   let index;
 
   function generateSuggestedPayments(totalPrice) {
-    const denominations = [500, 1000, 2000, 5000, 10000, 20000, 50000, 100000];
+    const denominations = [1, 2, 5, 10, 20, 50, 100, 200, 500];
     const suggestions = [];
 
     for (let denom of denominations) {
